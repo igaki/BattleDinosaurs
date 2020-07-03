b7472c9d2224ff4250fa1eae021cf7dece810ad3
Hiroshi Igaki <github@musictale.net>
2020-07-03 13:34:12 +0900

    add initlibrary

---
+++ BattleDinosaurs.java
@@ -0,0 +1,34 @@
+public class BattleDinosaurs {
+  String[] dinoName = new String[5];
+  int[] attackPoint = new int[5];
+  int[] agilityPoint = new int[5];
+  int[] defencePoint = new int[5];
+
+  void initLibrary() {
+    this.dinoName[0] = "ティラノ";
+    this.attackPoint[0] = 3;
+    this.agilityPoint[0] = 1;
+    this.defencePoint[0] = 1;
+
+    this.dinoName[0] = "ヴェロキラプトル";
+    this.attackPoint[0] = 1;
+    this.agilityPoint[0] = 3;
+    this.defencePoint[0] = 1;
+
+    this.dinoName[0] = "トリケラトプス";
+    this.attackPoint[0] = 1;
+    this.agilityPoint[0] = 1;
+    this.defencePoint[0] = 3;
+
+    this.dinoName[0] = "プテラノドン";
+    this.attackPoint[0] = 2;
+    this.agilityPoint[0] = 2;
+    this.defencePoint[0] = 1;
+
+    this.dinoName[0] = "ステゴサウルス";
+    this.attackPoint[0] = 2;
+    this.agilityPoint[0] = 1;
+    this.defencePoint[0] = 2;
+
+  }
+}
